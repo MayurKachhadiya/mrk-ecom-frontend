f5e7991afd4ccef22ff14840bf747da5bbf6e697
MayurKachhadiya <104882223+MayurKachhadiya@users.noreply.github.com>
2025-08-28 15:32:41 +0530

    final test

--- src/components/RoutesManage.jsx
+++ src/components/RoutesManage.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import HomePage from './HomePage';
 import AddProduct from './admin/AddProduct';
@@ -10,47 +10,45 @@ import ProductDetails from './ProductDetails';
 import CartDetails from './CartDetails';
 import Profile from './Profile';
 
-const RoutesManage =()=>{
-    return(
-        <>
-        <Router>
-          <Routes>
-            <Route path="/" element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            } />
-            <Route path="/addproduct" element={
-              <PrivateRoute>
-                <AddProduct />
-              </PrivateRoute>
-            } />
-            <Route path="/editproduct/:pid" element={
-              <PrivateRoute>
-                <EditProduct />
-              </PrivateRoute>
-            } />
-            <Route path="/product/:pid" element={
-              <PrivateRoute>
-                <ProductDetails />
-              </PrivateRoute>
-            } />
-            <Route path="/cart" element={
-              <PrivateRoute>
-                <CartDetails />
-              </PrivateRoute>
-            } />
-            <Route path="/profile" element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            } />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-        </>
-    )
+const RoutesManage = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        } />
+        <Route path="/addproduct" element={
+          <PrivateRoute>
+            <AddProduct />
+          </PrivateRoute>
+        } />
+        <Route path="/editproduct/:pid" element={
+          <PrivateRoute>
+            <EditProduct />
+          </PrivateRoute>
+        } />
+        <Route path="/product/:pid" element={
+          <PrivateRoute>
+            <ProductDetails />
+          </PrivateRoute>
+        } />
+        <Route path="/cart" element={
+          <PrivateRoute>
+            <CartDetails />
+          </PrivateRoute>
+        } />
+        <Route path="/profile" element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        } />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
 }
 export default RoutesManage;
